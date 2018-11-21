26c9fb50552efbc767bc4a5d57d8bb5e9595fc48
longforus <longforus@126.com>
2018-11-21 16:15:27 +0800

    add auto generate README.md

---
+++ autoREADME.sh
@@ -0,0 +1,17 @@
+echo "" >README.md
+path=""
+suffix=".md"
+for files in `find  ./ -name "*.md"`
+do
+    if [[ $path ]]; then
+        path="$path $files"
+    else
+        path=$files
+    fi
+    if [[ $files =~ $suffix ]]; then
+        echo $path
+        name=${path:2:${#str}-3}
+        echo "[$name]($path)" >> README.md
+        path=""
+    fi
+done
